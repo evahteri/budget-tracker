8747b53efe32c325f3cff25589827f13fa427569
Eero Vahteri <eero.vahteri@helsinki.fi>
2026-01-16 19:18:31 +0200

    add edit function to hooks

--- frontend/budget-tracker/src/hooks/useExpenses.js
+++ frontend/budget-tracker/src/hooks/useExpenses.js
@@ -3,38 +3,46 @@ import { useEffect, useState } from 'react'
 import { getExpenses, postExpense, putExpense } from '../api/expenses.api'
 
 export function useExpenses() {
-  const expenseQuery = useQuery({
-    queryKey: ['expenses'],
-    queryFn: () => getExpenses('all'),
-  })
+    const expenseQuery = useQuery({
+        queryKey: ['expenses'],
+        queryFn: () => getExpenses('all'),
+    })
 
-  const { data: expensesData = [], isLoading, error } = expenseQuery
+    const { data: expensesData = [], isLoading, error } = expenseQuery
 
-  const [expenses, setExpenses] = useState([])
+    const [expenses, setExpenses] = useState([])
 
-  useEffect(() => {
-    setExpenses(expensesData)
-  }, [expensesData])
+    useEffect(() => {
+        setExpenses(expensesData)
+    }, [expensesData])
 
-  const addExpense = async (event) => {
-    event.preventDefault()
+    const addExpense = async (event) => {
+        event.preventDefault()
 
-    const newExpense = {
-      description: event.target[0].value,
-      category: event.target[1].value,
-      amount: parseFloat(event.target[2].value),
+        const newExpense = {
+            description: event.target[0].value,
+            category: event.target[1].value,
+            amount: parseFloat(event.target[2].value),
+        }
+
+        const responseData = await postExpense(newExpense)
+        newExpense.id = responseData.id
+
+        setExpenses((prev) => [...prev, newExpense])
     }
 
-    const responseData = await postExpense(newExpense)
-    newExpense.id = responseData.id
+    const editExpense = async (event) => {
+        const newExpense = event
+        newExpense.amount = parseFloat(newExpense.amount) + 10  // Just for testing
+        const responseData = await putExpense(newExpense.id, newExpense)
+        setExpenses((prev) => prev.map(exp => exp.id === newExpense.id ? responseData : exp))
+    }
 
-    setExpenses((prev) => [...prev, newExpense])
-  }
-
-  return {
-    expenses,
-    isLoading,
-    error,
-    addExpense,
-  }
+    return {
+        expenses,
+        isLoading,
+        error,
+        addExpense,
+        editExpense,
+    }
 }
